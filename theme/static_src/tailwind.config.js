6c034125fcca4f7193a06c6474dac08adac72414
student950 <dhanavadaisheta07@gmail.com>
2026-02-23 19:56:01 +0530

    Initial commit - Django Taiwind DataShare project

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+        content: [
+    "../../templates/**/*.html",
+    "../../core/templates/**/*.html",
+    "../../users/templates/**/*.html",
+    "../../transfers/templates/**/*.html",
+],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
