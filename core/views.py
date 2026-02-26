23c1e6c37b37371f63974f6923da32d563c1c84f
student950 <dhanavadaisheta07@gmail.com>
2026-02-26 23:09:03 +0530

    Started working on Transfer Page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'core/home.html')
+
+def transfer(request):
+    return render(request,'core/transfer.html')
--- network_transfer/urls.py
+++ network_transfer/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, transfer
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',home,name='home'),
+    path('transfer/',transfer,name='transfer'),
 ]
