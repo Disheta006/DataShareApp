a9e351b7afdca353f2352ae9895d776f25cab10b
student950 <dhanavadaisheta07@gmail.com>
2026-02-27 15:06:43 +0530

    Completed basic Transfer page UI design

--- core/views.py
+++ core/views.py
@@ -5,4 +5,8 @@ def home(request):
     return render(request,'core/home.html')
 
 def transfer(request):
-    return render(request,'core/transfer.html')
+    data_options = ["100MB", "250MB", "500MB", "1GB", "2GB", "5GB"]
+    context = {
+        'data_options': data_options
+    }
+    return render(request,'core/transfer.html', context)
